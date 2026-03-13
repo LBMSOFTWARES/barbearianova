9e84fee5fb6f94dff63292abe2074f5f4e54c547
LBMSOFTWARES <softwareslbm@gmail.com>
2026-03-13 14:47:04 -0300

    Update settings.py

--- newsitebarb/settings.py
+++ newsitebarb/settings.py
@@ -64,8 +64,15 @@ WSGI_APPLICATION = 'newsitebarb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres.jrglgmtohfqnjrwrumqp'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'DpkG-3712-KeyAdm'),
+        'HOST': os.getenv('DB_HOST', 'aws-1-sa-east-1.pooler.supabase.com'),
+        'PORT': os.getenv('DB_PORT', '6543'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
