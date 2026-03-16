0a8ebc602a16ec79963f7ef901bfb93e46ada72e
LBMSOFTWARES <softwareslbm@gmail.com>
2026-03-15 23:23:19 -0300

    Update dados.py

--- newsitebarb/dados.py
+++ newsitebarb/dados.py
@@ -1,2 +1,2 @@
-ENDERECO = "Rua Exemplo, 123 - Centro"
-LINK_MAPA = "https://maps.google.com/..."
+ENDERECO = "R. Botucaraí, 542 - Q.ta da Boa Vista"
+LINK_MAPA = "https://maps.app.goo.gl/mPWfuYEcEVumyutXA"
